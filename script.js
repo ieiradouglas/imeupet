d52f011e062e6519b9ae11bf4d53e2a83c774710
Douglas Vieira da Silva <44785888+ieiradouglas@users.noreply.github.com>
2021-03-16 09:49:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+mudarEstado = (el) => {
+    var display = document.getElementById(el).style.display;
+    if(display == 'none'){
+        document.getElementById(el).style.display = "flex";
+        
+    }
+    else{
+        document.getElementById(el).style.display = "none";
+    }
+}
+
